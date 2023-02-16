e67aa6b728f3b21fd4a90d920710a75ec2a53ec5
impelcrypto <impelcrypto@gmail.com>
2023-02-16 21:55:47 +0800

    feat: enables dark mode

--- quasar.conf.js
+++ quasar.conf.js
@@ -98,7 +98,7 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
       config: {
-        dark: 'auto',
+        dark: true,
       },
 
       iconSet: 'material-icons', // Quasar icon set
