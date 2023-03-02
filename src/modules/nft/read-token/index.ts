90646b75ebcc6f211b52668b732a79d288c09134
Bobo <bobo.kovacevic@gmail.com>
2023-03-02 14:06:17 +0100

    Gas limit fix

--- src/modules/nft/read-token/index.ts
+++ src/modules/nft/read-token/index.ts
@@ -11,15 +11,6 @@ import { sanitizeIpfsUrl } from 'src/modules/nft/ipfs';
 import { IdBuilder } from 'src/modules/nft/rmrk-contract';
 import Contract from '../rmrk-contract/types/contracts/rmrk_contract';
 
-// "refTime: 4900005895"
-// "proofSize: 42866"
-const REF_TIME = 9480453976;
-const PROOF_SIZE = 531072;
-// const REF_TIME = 4900005895;
-// const PROOF_SIZE = 42866;
-// const REF_TIME = 4690845427;
-// const PROOF_SIZE = 1208;
-
 interface EquippableData {
   equippableGroupId: string;
   assetUri: string;
@@ -289,10 +280,10 @@ const getRmrkContract = ({ api, address }: { api: ApiPromise; address: string })
   const contract = new ContractPromise(api, abi, address);
   if (!contract || contract === null) new Error('There is no contract found');
 
-  const initialGasLimit = contract.registry.createType('WeightV2', {
-    proofSize: PROOF_SIZE,
-    refTime: REF_TIME,
-  });
+  const initialGasLimit = contract.registry.createType(
+    'WeightV2',
+    api.consts.system.blockWeights['maxBlock']
+  );
   return { contract, initialGasLimit };
 };
 
@@ -320,10 +311,13 @@ export const equipSlot = async ({
     childAssetId
   );
 
-  const gasLimit = api.registry.createType('WeightV2', gasRequired) as WeightV2;
-  console.log('gasLimit', gasLimit.toString());
   const transaction = contract.tx['equippable::equip'](
-    { gasLimit: gasLimit.refTime.toBn().muln(2) },
+    {
+      gasLimit: contract.registry.createType('WeightV2', {
+        proofSize: gasRequired.proofSize.toBn().muln(2),
+        refTime: gasRequired.refTime.toBn().muln(2),
+      }),
+    },
     IdBuilder.U64(tokenId.u64 ?? 0),
     assetId,
     slot,
