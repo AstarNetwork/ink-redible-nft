b27454316fe4cf7dd3fce05146d4b33d432dee45
Bobo <bobo.kovacevic@gmail.com>
2023-06-08 14:11:01 +0200

    Stucked with Appolo

--- src/hooks/useCollectionInfo.ts
+++ src/hooks/useCollectionInfo.ts
@@ -1,19 +1,65 @@
+import { watch } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
+import { useNetworkInfo } from './useNetworkInfo';
 
-// TODO filter by network
 export const useCollectionInfo = () => {
-  const { result, loading, error } = useQuery(gql`
-    query CommentsByContract {
-      posts(where: { rootPost: { space: { id_eq: "11453" } } }) {
-        id
-        image
-        title
-        canonical
-        body
+  const { currentNetworkName } = useNetworkInfo();
+  // const { result, loading, error, refetch } = useQuery(
+  //   gql`
+  //     query CommentsByContract {
+  //       posts(
+  //         where: {
+  //           rootPost: {
+  //             space: { id_eq: "11453" }
+  //             hidden_not_eq: true
+  //             tagsOriginal_containsInsensitive: "${currentNetworkName.value}"
+  //           }
+  //         }
+  //       ) {
+  //         id
+  //         image
+  //         title
+  //         canonical
+  //         body
+  //       }
+  //     }
+  //   `
+  // );
+
+  const { result, loading, error, refetch } = useQuery(
+    gql`
+      query CommentsByContract($networkName: String!) {
+        posts(
+          where: {
+            rootPost: {
+              space: { id_eq: "11453" }
+              hidden_not_eq: true
+              tagsOriginal_containsInsensitive: $networkName
+            }
+          }
+        ) {
+          id
+          image
+          title
+          canonical
+          body
+        }
       }
-    }
-  `);
+    `,
+    { variables: { $networkName: 'Astar' } }
+  );
+
+  watch(
+    currentNetworkName,
+    () => {
+      console.log(currentNetworkName.value);
+      // if (currentNetworkName.value) {
+      //   refetch({ variables: { $networkName: currentNetworkName.value } });
+      // }
+    },
+    { immediate: true }
+  );
 
   return { result, loading, error };
 };
